f5201cb947f7e9222def58fc5da87e6d8179da7b
Vitaliy Peteliuk <ernestocarmonegg@gmail.com>
2025-04-17 19:22:48 -0500

    3122

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connectDB()
 app.use(cookieParser())
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
