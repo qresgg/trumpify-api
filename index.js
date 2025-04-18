18937cc8b029a9c673baaa3ff114561ab2818819
Vitaliy Peteliuk <ernestocarmonegg@gmail.com>
2025-04-17 19:16:55 -0500

    й4323

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ connectDB()
 
 app.use(cookieParser())
 app.use((req, res, next) => {
-  res.setHeader("Accept-Control-Allow-Origin", "*");
-  res.setHeader("Accept-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Accept-Control-Allow-Headers", "Content-Type, Authorization");
-  next()
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
 app.use(session({
