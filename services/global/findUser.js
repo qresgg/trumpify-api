11c1ea6997335b0d9df709e186bb21ee915fbaa9
Vitaliy Peteliuk <ernestocarmonegg@gmail.com>
2025-04-17 20:15:52 -0500

    21312

--- services/global/findUser.js
+++ services/global/findUser.js
@@ -1,7 +1,9 @@
 const User = require('../../models/User/UserModel');
+const { ObjectId } = require('mongodb');
+
 
 const findUserById = async (userId) => {
-    const user = await User.findById(userId);
+    const user = await User.findById(new ObjectId(userId));
     if (!user) {
         throw new Error('User not found');
     }
