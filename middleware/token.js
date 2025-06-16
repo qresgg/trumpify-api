dda2440b3f8ce97dc055a71003b786ee4c7d7cd8
Vitaliy Peteliuk <ernestocarmonegg@gmail.com>
2025-06-16 02:54:25 -0500

    hotfix

--- middleware/token.js
+++ middleware/token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { SECRETKEY_ACCESS, SECRETKEY_REFRESH } = process.env;
 
 const generateAccessToken = (user) => {
-    return jwt.sign({id: user._id, userName: user.user_name}, SECRETKEY_ACCESS, { expiresIn: '15m'})
+    return jwt.sign({id: user._id, userName: user.user_name}, SECRETKEY_ACCESS, { expiresIn: '60m'})
 }
 const generateRefreshToken = (user) => {
     return jwt.sign({id: user._id, userName: user.user_name}, SECRETKEY_REFRESH, { expiresIn: '7d' })
